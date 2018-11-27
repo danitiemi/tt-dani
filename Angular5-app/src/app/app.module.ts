40bc1762224419e5fbc7424bbc239f19676a59f8
danitiemi <danitiemi.br@gmail.com>
2018-11-27 12:43:45 -0800

    Angular App setup

---
+++ Angular5-app/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { UploadImageComponent } from './upload-image/upload-image.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UploadImageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
