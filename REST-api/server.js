86b48f3c7818d2d3dd3f0fe2a951bded62d33980
danitiemi <danitiemi.br@gmail.com>
2018-11-27 14:35:19 -0800

    server setup

--- REST-api/server.js
+++ REST-api/server.js
@@ -1,1 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
+const app = express();
+const PORT = 8000;
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// CORS setup
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+app.get('/', (req, res) =>
+  res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () => {
+  console.log(`Your server is running on port ${PORT}`);
+})
